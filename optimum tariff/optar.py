d3ec7741e13b05eeca2c931bbef2cda5f2a133e0
seoykwon <kwonseoy@gmail.com>
2024-07-21 12:45:21 +0900

    filled pi, alpha, gamma values

--- optimum tariff/optar.py
+++ optimum tariff/optar.py
@@ -22,28 +22,34 @@ pol_econ = {country: {industry: np.random.rand() for industry in industries} for
 x = {country: {industry: np.random.rand() for industry in industries} for country in countries}
 
 # used when calculating welfare
-p_is = {'China': {'Germany': {'gim': 11.113849080349386, 'steel': 1.0582132952603989, 'semi': 0, 'semi_memory': 0.8749017013649593, 'car': 22347.52148025721}, 
-                  'Japan': {'gim': 4.702912149549132, 'steel': 0.6545231939214761, 'semi': 0, 'semi_memory': 1.3782939026477783, 'car': 28876.362273677052}, 
-                  'Korea': {'gim': 1.9437738767665877, 'steel': 0.6248022737655794, 'semi': 0, 'semi_memory': 3.2280803660391775, 'car': 27254.069456311554}, 
-                  'USA': {'gim': 7.338449268069469, 'steel': 1.2736582321419347, 'semi': 0, 'semi_memory': 1.4443405822490798, 'car': 6412.672705308079}}, 
-        'Germany': {'China': {'gim': 38.2548102189781, 'steel': 1.380815010542162, 'semi': 0, 'semi_memory': 0.2344066978065146, 'car': 67597.35761649995}, 
-                    'Japan': {'steel': 1.8565627953727868, 'semi': 0, 'semi_memory': 0.8579526696580784, 'car': 43312.870251408094}, 
-                    'Korea': {'gim': 193.40293333333332, 'steel': 2.0382712213649143, 'semi': 0, 'semi_memory': 3.0795445270513784, 'car': 53270.511687146936}, 
-                    'USA': {'gim': 144.169, 'steel': 0.9205237153804969, 'semi': 0, 'semi_memory': 1.1508868485166053, 'car': 63936.47417704994}}, 
-        'Japan': {'China': {'gim': 8.886296287788753, 'steel': 0.7661557893436499, 'semi': 0, 'semi_memory': 1.9214838065500246, 'car': 34435.358268213306}, 
-                  'Germany': {'gim': 30.806603908794788, 'steel': 1.009475489607891, 'semi': 0, 'semi_memory': 5.56617554347278, 'car': 24246.767236564578}, 
-                  'Korea': {'gim': 11.861566270365477, 'steel': 0.6452938452728846, 'semi': 0, 'semi_memory': 2.576506085798766, 'car': 25945.71517654626}, 
-                  'USA': {'gim': 37.98874077567761, 'steel': 0.6924101202128765, 'semi': 0, 'semi_memory': 5.473356193407144, 'car': 27683.734550894653}}, 
-        'USA': {'China': {'steel': 2.793492934160387, 'semi': 0, 'semi_memory': 1.8820447349060032, 'car': 59753.32634173833, 'gim': 202.66666666666666}, 
-                'Germany': {'gim': 10.990251530053763, 'steel': 0.9743858548405673, 'semi': 0, 'semi_memory': 20.99480847346542, 'car': 45214.10895838236}, 
-                'Japan': {'gim': 12.888821246314357, 'steel': 3.19057747932092, 'semi': 0, 'semi_memory': 1.4593678187228334, 'car': 51928.2106433143}, 
-                'Korea': {'gim': 17.13888888888889, 'steel': 3.5832969128690055, 'semi': 0, 'semi_memory': 3.497620193153412, 'car': 47727.025701300736}}, 
-        'Korea': {'China': {'gim': 11.346897999992917, 'steel': 0.8095350234608575, 'semi': 0, 'semi_memory': 2.9933717074944526, 'car': 24573.867215686274}, 
-                  'Germany': {'gim': 12.498771320251953, 'steel': 1.0443284707421892, 'semi': 0, 'semi_memory': 0, 'car': 31550.779572362917}, 
-                  'Japan': {'gim': 10.377253137100842, 'steel': 0.760506783231565, 'semi': 0, 'semi_memory': 2.1596662328607987, 'car': 18812.184776746457}, 
-                  'USA': {'gim': 16.36964135872177, 'steel': 0.9889283307131168, 'semi': 0, 'semi_memory': 6.354314554989172, 'car': 24121.347339170934}
-                 }            
-        }
+p_is = {
+    'China': {
+        'Germany': {'gim': 11.113849080349386, 'steel': 1.0582132952603989, 'semi': 0, 'semi_memory': 0.8749017013649593, 'car': 22347.52148025721}, 
+        'Japan': {'gim': 4.702912149549132, 'steel': 0.6545231939214761, 'semi': 0, 'semi_memory': 1.3782939026477783, 'car': 28876.362273677052}, 
+        'Korea': {'gim': 1.9437738767665877, 'steel': 0.6248022737655794, 'semi': 0, 'semi_memory': 3.2280803660391775, 'car': 27254.069456311554}, 
+        'USA': {'gim': 7.338449268069469, 'steel': 1.2736582321419347, 'semi': 0, 'semi_memory': 1.4443405822490798, 'car': 6412.672705308079}}, 
+    'Germany': {
+        'China': {'gim': 38.2548102189781, 'steel': 1.380815010542162, 'semi': 0, 'semi_memory': 0.2344066978065146, 'car': 67597.35761649995}, 
+        'Japan': {'steel': 1.8565627953727868, 'semi': 0, 'semi_memory': 0.8579526696580784, 'car': 43312.870251408094}, 
+        'Korea': {'gim': 193.40293333333332, 'steel': 2.0382712213649143, 'semi': 0, 'semi_memory': 3.0795445270513784, 'car': 53270.511687146936}, 
+        'USA': {'gim': 144.169, 'steel': 0.9205237153804969, 'semi': 0, 'semi_memory': 1.1508868485166053, 'car': 63936.47417704994}}, 
+    'Japan': {
+        'China': {'gim': 8.886296287788753, 'steel': 0.7661557893436499, 'semi': 0, 'semi_memory': 1.9214838065500246, 'car': 34435.358268213306}, 
+        'Germany': {'gim': 30.806603908794788, 'steel': 1.009475489607891, 'semi': 0, 'semi_memory': 5.56617554347278, 'car': 24246.767236564578}, 
+        'Korea': {'gim': 11.861566270365477, 'steel': 0.6452938452728846, 'semi': 0, 'semi_memory': 2.576506085798766, 'car': 25945.71517654626}, 
+        'USA': {'gim': 37.98874077567761, 'steel': 0.6924101202128765, 'semi': 0, 'semi_memory': 5.473356193407144, 'car': 27683.734550894653}}, 
+    'USA': {
+        'China': {'steel': 2.793492934160387, 'semi': 0, 'semi_memory': 1.8820447349060032, 'car': 59753.32634173833, 'gim': 202.66666666666666}, 
+        'Germany': {'gim': 10.990251530053763, 'steel': 0.9743858548405673, 'semi': 0, 'semi_memory': 20.99480847346542, 'car': 45214.10895838236}, 
+        'Japan': {'gim': 12.888821246314357, 'steel': 3.19057747932092, 'semi': 0, 'semi_memory': 1.4593678187228334, 'car': 51928.2106433143}, 
+        'Korea': {'gim': 17.13888888888889, 'steel': 3.5832969128690055, 'semi': 0, 'semi_memory': 3.497620193153412, 'car': 47727.025701300736}}, 
+    'Korea': {
+        'China': {'gim': 11.346897999992917, 'steel': 0.8095350234608575, 'semi': 0, 'semi_memory': 2.9933717074944526, 'car': 24573.867215686274}, 
+        'Germany': {'gim': 12.498771320251953, 'steel': 1.0443284707421892, 'semi': 0, 'semi_memory': 0, 'car': 31550.779572362917}, 
+        'Japan': {'gim': 10.377253137100842, 'steel': 0.760506783231565, 'semi': 0, 'semi_memory': 2.1596662328607987, 'car': 18812.184776746457}, 
+        'USA': {'gim': 16.36964135872177, 'steel': 0.9889283307131168, 'semi': 0, 'semi_memory': 6.354314554989172, 'car': 24121.347339170934}
+        }            
+    }
 
 P_j = {'Germany':131.8924482,
         'China':132.2291519,
@@ -52,8 +58,7 @@ P_j = {'Germany':131.8924482,
         'USA':139.7357936
 }
 
-#계산가능
-gamma = {i: {j: {industry: np.random.rand() for industry in industries} for j in countries} for i in countries}
+
 
 # tau = {i: {j: {industry: np.random.rand() + 1 for industry in industries} for j in countries} for i in countries}
 tau = {
@@ -113,17 +118,20 @@ t = {'Korea': {'USA': {'gim': 0, 'steel': 0, 'semi': 0, 'car': 0},
                 'Japan': {'gim': 0.4, 'steel': 0, 'semi': 0, 'car': 0}, 
                 'China': {'gim': 0.2, 'steel': 0.05, 'semi': 0, 'car': 0.085}}}
 
-#계산가능
-pi = {country: {industry: np.random.rand() for industry in industries} for country in countries}
+#계산함
+pi = {country: {industry: 0 for industry in industries} for country in countries}
+alpha = {i: {j: {industry: 0 for industry in industries} for j in countries} for i in countries}
+gamma = {i: {j: {industry: 0 for industry in industries} for j in countries} for i in countries}
 
-pi_hat = {country: {industry: np.random.rand() for industry in industries} for country in countries}
+
 #계산가능
-alpha = {i: {j: {industry: np.random.rand() for industry in industries} for j in countries} for i in countries}
+pi_hat = {country: {industry: np.random.rand() for industry in industries} for country in countries}
 tau_hat = {i: {j: {industry: np.random.rand() + 1 for industry in industries} for j in countries} for i in countries}
 t_hat = {i: {j: {industry: np.random.rand() + 1 for industry in industries} for j in countries} for i in countries}
 
 #원자료 80%
-w_hat = {country: 1 for country in countries}
+# w_hat = {country: 1 for country in countries}
+w_hat = {'Korea':16.64, 'USA':33.7, 'China':6.21, 'Japan':12.33, 'Germany':25.34}
 
 
 T = {
@@ -153,6 +161,56 @@ T = {
               'USA': {'gim': 33963781.0, 'steel': 581315367.0, 'semi': 2400493160.0, 'semi_memory': 236493837.0, 'car': 883492635.0}
             }
     }
+
+gamma_denom = {j: {industry: 0 for industry in industries} for j in countries}
+
+def fill_gamma_denom():
+    for j in countries:
+        for s in industries:
+            # gamma_denom[j][s] = 0
+            for m in countries:
+                if m != j:
+                    gamma_denom[j][s] += tau[m][j][s] * T[m][j][s]
+                
+
+def fill_gamma():
+    fill_gamma_denom()
+    for i in countries:
+        for j in countries:
+            for s in industries:
+                if i != j:
+                    gamma[i][j][s] = tau[i][j][s] * T[i][j][s] / gamma_denom[j][s]
+                
+
+fill_gamma()
+
+def fill_pi():
+    for j in countries:
+        for s in industries:
+            for i in countries:
+                if i != j:
+                    pi[j][s] += 1/sigma[s] * T[i][j][s]
+
+fill_pi()
+
+alpha_denom = {j: {industry: 0 for industry in industries} for j in countries}
+
+def fill_alpha_denom():
+    for i in countries:
+        for s in industries:
+            for n in countries:
+                if i != n:
+                    alpha_denom[i][s] += T[i][n][s]            
+
+def fill_alpha():
+    fill_alpha_denom()
+    for i in countries:
+        for j in countries:
+            for s in industries:
+                if i != j:
+                    alpha[i][j][s] += T[i][j][s] / alpha_denom[i][s]               
+
+fill_alpha()
 
 # Welfare function
 def welfare(j, s):
